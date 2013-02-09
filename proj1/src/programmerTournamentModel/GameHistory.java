fa1372d40d8027523b49a15c00a63482c42cf692
andrewns <andrewsoderquist@gmail.com>
2013-02-09 15:10:47 -0700

    Finished all the javadoc!!! Until next week...

--- proj1/src/programmerTournamentModel/GameHistory.java
+++ proj1/src/programmerTournamentModel/GameHistory.java
@@ -6,15 +6,25 @@ import java.util.List;
 import liarsDiceModel.Result;
 import liarsDiceModel.Round;
 import liarsDiceModel.Turn;
-//done
+
+/**
+ * Keeps track of the history of a game (in the form of a collection of Rounds, which are a collection of Turns).
+ */
 public class GameHistory {
 	private List<Round> rounds;
 	
+	/**
+	 * Constructor.
+	 */
 	public GameHistory(){
 		rounds = new ArrayList<Round>();
 		rounds.add(new Round());
 	}
 	
+	/**
+	 * Copy constructor. (Makes a deep copy.)
+	 * @param gh The GameHistory object to be copied.
+	 */
 	public GameHistory(GameHistory gh){
 		//deep copy:
 		List<Round> tempList = new ArrayList<Round>();
@@ -24,18 +34,33 @@ public class GameHistory {
 		rounds = tempList;
 	}
 
+	/**
+	 * @return List of this game's Rounds.
+	 */
 	public List<Round> getRounds() {
 		return rounds;
 	}
 
+	/**
+	 * Adds a Round to the history.
+	 * @param round Round to be added.
+	 */
 	public void addRound(Round round){
 		rounds.add(round);
 	}
 	
+	/**
+	 * Adds a Turn to the current Round in the history.
+	 * @param round Turn to be added.
+	 */
 	public void addTurn(Turn turn){
 		rounds.get(rounds.size() - 1).addTurn(turn);
 	}
 	
+	/**
+	 * Ends the current Round with the given Result.
+	 * @param result End result of the current Round.
+	 */
 	public void endRound(Result result){
 		rounds.get(rounds.size() - 1).end(result);
 	}
--- proj1/src/programmerTournamentModel/HumanController.java
+++ proj1/src/programmerTournamentModel/HumanController.java
@@ -4,13 +4,22 @@ import liarsDiceModel.Decision;
 import liarsDiceModel.GameInfo;
 import liarsDiceModel.LiarsDiceBot;
 
-
+/**
+ * This class will interface with the player view to get the human player's input.
+ */
 public class HumanController extends LiarsDiceBot {
 
+	/**
+	 * @return The name of the human player.
+	 */
 	public String getName() {
 		return "HumanPlayer";
 	}
 
+	/**
+	 * @param currentGameInfo The current state of the game. (Probably don't need this - human can see it.)
+	 * @return The human's decision.
+	 */
 	public Decision getDecision(GameInfo currentGameInfo) {
 		//TODO stubbed out
 		//will need to talk up at the View level to get the human input
