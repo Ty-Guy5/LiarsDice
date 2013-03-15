8917ad57350499ec8cd13ec7519812d2d2c95fe0
Ty-Guy5 <wisety@gmail.com>
2013-03-15 12:59:01 -0600

    Changed the PlayerInfo object. Broken build currently

--- proj1/src/model/liarsDice/gameInfo/PlayerInfo.java
+++ proj1/src/model/liarsDice/gameInfo/PlayerInfo.java
@@ -1,39 +1,59 @@
 package model.liarsDice.gameInfo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import model.liarsDice.gameLogic.Die;
 import model.liarsDice.gameLogic.LiarsDicePlayer;
 
 
 /**
- * Information about a player which a player knows throughout the game.  (May be used to make decisions on their turns.)
+ * Information about a player which a player knows throughout the game.  
+ * (May be used to make decisions on their turns.)
  */
 public class PlayerInfo {
-	private int numDice;
+	private List<Die> dice;
 	private int id;
+	private boolean diceHidden;
 	
 	/**
 	 * Constructor.
-	 * @param numDice The number of dice this player still has.
+	 * @param dice The number of dice this player still has.
 	 * @param playerID The player's unique ID.
 	 */
-	public PlayerInfo(int numDice, int playerID) {
-		this.numDice = numDice;
+	public PlayerInfo(List<Die> dice, int playerID, boolean diceHidden) {
+		this.dice = dice;
 		this.id = playerID;
+		this.diceHidden = diceHidden;
 	}
 	
 	/**
-	 * "Copy" constructor.
+	 * "Copy" constructor for creating a PlayerInfo object from a player.
 	 * @param p The player about whom we are creating this PlayerInfo object.
 	 */
-	public PlayerInfo(LiarsDicePlayer p){
-		numDice = p.getDice().size();
+	public PlayerInfo(LiarsDicePlayer p, boolean diceHidden){
+		dice = new ArrayList<Die>();
+		Collections.copy(dice, p.getDice());
 		id = p.getID();
+		this.diceHidden = diceHidden;
 	}
 
 	/**
 	 * @return The number of dice this player still has.
 	 */
 	public int getNumDice() {
-		return numDice;
+		return dice.size();
+	}
+	
+	/**
+	 * @return The player's dice, or null if the player's dice are hidden.
+	 */
+	public List<Die> getDice() {
+		if (diceHidden)
+			return null;
+		else
+			return dice;
 	}
 
 	/**
